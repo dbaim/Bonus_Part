65577b8b3086451dd59f6f699df69aa99ccae212
dbaim <79461183+dbaim@users.noreply.github.com>
2022-11-06 12:30:04 +0600

    First commit

--- csci341/csci341/settings.py
+++ csci341/csci341/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 import whitenoise
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,6 +84,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
